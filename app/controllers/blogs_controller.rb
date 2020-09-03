a093a3e5df9208482de098e61a9403ec9cb6e06c
EC2 Default User <ec2-user@ip-172-31-46-112.ec2.internal>
2020-09-03 06:11:04 +0000

    [FINISH] 2-7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,22 +1,39 @@
 class BlogsController < ApplicationController
   def index
     @blogs = Blog.all
+    p "標準出力にのみ反映"
+    logger.debug("標準出力とログファイルに反映される")
   end
 
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def new
-    @blog1 = Blog.new
+    @blog = Blog.new
   end
 
   def create
-    list = Blog.new(blog_params)
-    list.save
-    redirect_to blogs_path
+    blog = Blog.new(blog_params)
+    binding.pry
+    blog.save
+    redirect_to blog_path(blog.id)
   end
 
   def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog.id)
+  end
+
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
   end
 
   private
